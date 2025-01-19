4d53fb5efa47a0d543941b6728ab3c2515c7c918
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-19 15:50:53 -0300

    Adicionar produto, assinatura, e servico e avalia-los

--- src/main/java/br/com/pratica/generics/GenericsApplication.java
+++ src/main/java/br/com/pratica/generics/GenericsApplication.java
@@ -1,5 +1,9 @@
 package br.com.pratica.generics;
 
+import br.com.pratica.generics.modelos.Assinatura;
+import br.com.pratica.generics.modelos.Produto;
+import br.com.pratica.generics.modelos.Servico;
+import br.com.pratica.generics.services.Avaliacao;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +17,13 @@ public class GenericsApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("teste");
+		Produto produto1 = new Produto("brinquedo");
+		Avaliacao<Produto> avaliacao1 = new Avaliacao<>(produto1, 4);
+
+		Assinatura assinatura1 = new Assinatura("streaming");
+		Avaliacao<Assinatura> avaliacao2 = new Avaliacao<>(assinatura1, 3.1);
+
+		Servico servico1 = new Servico("instalação");
+		Avaliacao<Servico> avaliacao3 = new Avaliacao<>(servico1, 5);
 	}
 }
